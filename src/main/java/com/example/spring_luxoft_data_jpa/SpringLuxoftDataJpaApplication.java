c2548111cbd464dfda7a00848d2a0aa9946c9d79
merdeevrz <merdeevrz@haulmont.com>
2021-08-25 15:10:33 +0400

    Initial commit

---
+++ src/main/java/com/example/spring_luxoft_data_jpa/SpringLuxoftDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_luxoft_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLuxoftDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLuxoftDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_luxoft_data_jpa/SpringLuxoftDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_luxoft_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLuxoftDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
